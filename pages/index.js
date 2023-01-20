330ef4dc826105f98f7361a9180b03684a160f71
Olga Mishareva <mishareva.olga@gmail.com>
2023-01-20 14:22:54 +0100

    feat: add maping of data from db.json & list styles

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,25 @@
+import styled from "styled-components";
+
+import items from "@/db.json";
+import Item from "@/components/Item";
+
 export default function HomePage() {
-  return <p>Hello from Next.js</p>;
+  return (
+    <StyledList>
+      {items.map((item) => (
+        <li key={item.itemId}>
+          <Item title={item.itemTitle} initialStatus={item.initiallyLost} />
+        </li>
+      ))}
+    </StyledList>
+  );
 }
+
+const StyledList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  row-gap: 30px;
+  margin: 0;
+  padding: 30px 0;
+`;
