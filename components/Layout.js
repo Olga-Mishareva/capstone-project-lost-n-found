8abb3c0ff78d48a2b344377061a5d2a5069d1e1b
Olga Mishareva <mishareva.olga@gmail.com>
2023-01-20 14:20:09 +0100

    feat: add styles for main & footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,44 @@
-import Head from "next/head";
 import styled from "styled-components";
 
 import { TitleBar } from "./TitleBar";
 
 export function Layout({ children }) {
   return (
-    <div>
-      <Head>
-        <title>Lost-n-Found</title>
-      </Head>
+    <Wrapper>
       <TitleBar></TitleBar>
-      <main>{children}</main>
-    </div>
+      <StyledMain>{children}</StyledMain>
+      <Footer>
+        <Copyright>&copy; OM</Copyright>
+      </Footer>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-rows: 70px auto 36px;
+  height: 100vh;
+`;
+
+const StyledMain = styled.main`
+  overflow-y: scroll;
+  margin: 0 auto;
+
+  ::-webkit-scrollbar {
+    width: 0;
+  }
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Copyright = styled.p`
+  margin: 0;
+  padding: 10px 20px;
+  font-size: 14px;
+  line-height: 16px;
+  color: rgba(26, 26, 26, 0.3);
+`;
