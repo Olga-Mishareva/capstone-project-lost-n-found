585384022e882a5f817448274e1f888d031c7b86
Olga Mishareva <mishareva.olga@gmail.com>
2023-01-20 14:18:28 +0100

    feat: add styles for headline

--- components/TitleBar.js
+++ components/TitleBar.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export function TitleBar() {
   return (
     <StyledHeader>
-      <StyledHeadline>Lost-n-Found</StyledHeadline>
+      <Headline>Lost-n-Found</Headline>
     </StyledHeader>
   );
 }
@@ -12,10 +12,12 @@ const StyledHeader = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
-  min-height: 50px;
-  border-bottom: 2px solid rgba(26, 26, 26, 0.2);
 `;
 
-const StyledHeadline = styled.h1`
-  font-weight: 800;
+const Headline = styled.h1`
+  font-weight: 700;
+  width: 85%;
+  border-bottom: 2px solid rgba(26, 26, 26, 0.3);
+  padding: 15px 0;
+  text-align: center;
 `;
