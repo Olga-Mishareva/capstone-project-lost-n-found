a933adf6d99d079b6b8ad8a2461ac69b3be4ee87
Olga Mishareva <mishareva.olga@gmail.com>
2023-01-20 14:17:29 +0100

    feat: add color vars for root

--- styles.js
+++ styles.js
@@ -7,8 +7,16 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  :root {
+    --lost-color: #ED5050;
+    --found-color: #3F7FCA;
+    --finished-color: #448C74;
+    --font-color: #262626;
+  }
+
   body {
     margin: 0;
     font-family: system-ui;
+    color: var(--font-color);
   }
 `;
