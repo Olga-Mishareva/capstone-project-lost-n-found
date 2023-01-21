7448f6d8f8d2338cb6e96db06c4964aeb97784b5
Olga Mishareva <mishareva.olga@gmail.com>
2023-01-21 16:41:51 +0100

    feat: add fetcher & swr-config

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,27 @@
 import Head from "next/head";
 import styled from "styled-components";
 import { Inter } from "@next/font/google";
+import { SWRConfig } from "swr";
 
 import GlobalStyle from "@/styles";
 import { TitleBar } from "@/components/TitleBar";
 import { Layout } from "@/components/Layout";
+import fetcher from "@/lib/fetcher";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className={inter.className}>
-      <Head>
-        <title>Lost-n-Found</title>
-      </Head>
-      <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </div>
+    <SWRConfig value={{ fetcher }}>
+      <div className={inter.className}>
+        <Head>
+          <title>Lost-n-Found</title>
+        </Head>
+        <GlobalStyle />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </div>
+    </SWRConfig>
   );
 }
