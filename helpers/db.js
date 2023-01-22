420183e238a6753a226122343ba9387ec873de1f
Olga Mishareva <mishareva.olga@gmail.com>
2023-01-22 20:59:09 +0100

    feat: add database connection, schema

---
+++ helpers/db.js
@@ -0,0 +1,33 @@
+import mongoose, { model, models, Schema } from "mongoose";
+
+const URI = `mongodb+srv://mooowik:${process.env.MONGODB_PASSWORD}@lost-n-found.dp2d557.mongodb.net/?retryWrites=true&w=majority`;
+
+const ItemSchema = new Schema({
+  initiallyLost: Boolean,
+  title: String,
+  description: String,
+  itemId: String,
+  userName: String,
+  userId: String,
+  userRole: String,
+  longitude: String,
+  latitude: String,
+  inDiscuss: Boolean,
+  isFinished: Boolean,
+  messages: [{ userName: String, userId: String, message: String }],
+});
+
+const Item = models.Item || model("Item", ItemSchema);
+
+async function connectDatabase() {
+  await mongoose.connect(URI);
+}
+
+async function getAllItems() {
+  await connectDatabase();
+
+  const items = await Item.find({});
+  return items;
+}
+
+export { getAllItems };
