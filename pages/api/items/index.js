2df43affc7d10163f17cb35cd390826d7ff1d77b
Olga Mishareva <mishareva.olga@gmail.com>
2023-01-23 14:29:04 +0100

    fix: naming requsrt / response

--- pages/api/items/index.js
+++ pages/api/items/index.js
@@ -1,10 +1,10 @@
 import { getAllItems } from "@/helpers/db";
 
-export default async function handler(req, res) {
-  switch (req.method) {
+export default async function handler(request, response) {
+  switch (request.method) {
     case "GET": {
       const items = await getAllItems();
-      res.status(200).json(items);
+      response.status(200).json(items);
       break;
     }
     default: {
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
         .status(405)
         .setHeader("Allow", "GET")
         .json(
-          `Error: request method ${req.method} on ${req.url} is not allowed `
+          `Error: request method ${request.method} on ${request.url} is not allowed `
         );
     }
   }
