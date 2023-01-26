74d537020d182968a692c37c6667d9dc77a9d1d7
Olga Mishareva <mishareva.olga@gmail.com>
2023-01-26 12:55:55 +0100

    feat: add area-label to svg

--- components/SVGIcon.js
+++ components/SVGIcon.js
@@ -1,10 +1,15 @@
-import { svgPaths } from "@/lib/paths";
+import { svgPaths } from "@/lib/svgPaths";
 
 export default function SVGIcon({ variant, width, color = "currentColor" }) {
   return (
-    <svg viewBox={paths[variant].viewbox} width={width} fill={color}>
-      <title>{variant}</title>
-      <path d={paths[variant].path} />
+    <svg
+      viewBox={svgPaths[variant].viewbox}
+      width={width}
+      fill={color}
+      aria-labelledby="title"
+    >
+      <title id="title">{variant}</title>
+      <path d={svgPaths[variant].path} />
     </svg>
   );
 }
