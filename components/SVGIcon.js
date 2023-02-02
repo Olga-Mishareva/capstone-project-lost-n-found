9c243cf295e991561c5fdded64443e6b4a10c7eb
Olga Mishareva <mishareva.olga@gmail.com>
2023-02-02 17:50:14 +0100

    feat: add svg icons for map & list

--- components/SVGIcon.js
+++ components/SVGIcon.js
@@ -3,6 +3,7 @@ import { svgPaths } from "@/lib/svgPaths";
 export default function SVGIcon({
   variant,
   width,
+  height,
   label,
   color = "currentColor",
 }) {
@@ -10,8 +11,10 @@ export default function SVGIcon({
     <svg
       viewBox={svgPaths[variant].viewbox}
       width={width}
+      height={height}
       fill={color}
       aria-label={label}
+      preserveAspectRatio="xMidYMid meet"
     >
       <title id={label}>{variant}</title>
       <path d={svgPaths[variant].path} />
