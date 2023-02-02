9e6e971d1f3befbf31a3b26c6858d7858b2df730
Olga Mishareva <mishareva.olga@gmail.com>
2023-02-02 17:50:52 +0100

    feat: add toggle function for view

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import { SWRConfig } from "swr";
+import { useState } from "react";
 
 import GlobalStyle from "@/styles";
 import { Layout } from "@/components/Layout";
@@ -9,6 +10,12 @@ import fetcher from "@/lib/fetcher";
 const inter = Inter({ subsets: ["latin"], variable: "--inter-font" });
 
 export default function App({ Component, pageProps }) {
+  const [listView, setListView] = useState(false);
+
+  function handleToggleView() {
+    setListView((prevView) => !prevView);
+  }
+
   return (
     <SWRConfig value={{ fetcher }}>
       <div className={inter.className}>
@@ -16,8 +23,8 @@ export default function App({ Component, pageProps }) {
           <title>Lost-n-Found</title>
         </Head>
         <GlobalStyle />
-        <Layout>
-          <Component {...pageProps} />
+        <Layout onToggle={handleToggleView} listView={listView}>
+          <Component {...pageProps} listView={listView} />
         </Layout>
       </div>
     </SWRConfig>
