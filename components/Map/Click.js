2a869f04001c7ae01980ca1359b7049821aa4d09
Olga Mishareva <mishareva.olga@gmail.com>
2023-02-07 10:52:06 +0100

    fix: move popup into ckick function

--- components/Map/Click.js
+++ components/Map/Click.js
@@ -1,11 +1,35 @@
 const { useMapEvents } = require("react-leaflet");
+import { Popup } from "react-leaflet";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
-export default function Click({ onPosition, setIsConfirmPopupOpen }) {
+import ConfirmPopup from "../ConfirmPopup";
+
+export default function Click({ onPosition }) {
+  const router = useRouter();
+  const [position, setPosition] = useState(null);
+
+  function handleConfirm() {
+    setPosition(null);
+    router.push("/create");
+  }
+
+  function handleClose() {
+    setPosition(null);
+  }
+
   const map = useMapEvents({
-    click: (e) => {
-      onPosition(e.latlng);
-      setIsConfirmPopupOpen(true);
+    click: (event) => {
+      onPosition(event.latlng);
+      setPosition(event.latlng);
     },
   });
-  return null;
+
+  return (
+    position && (
+      <Popup position={position} closeButton={false}>
+        <ConfirmPopup onConfirm={handleConfirm} onClose={handleClose} />
+      </Popup>
+    )
+  );
 }
--- components/Map/Map.js
+++ components/Map/Map.js
@@ -3,47 +3,20 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
 import styled from "styled-components";
-import { useRouter } from "next/router";
 
 import LocationMarker from "./LocationMarker";
 import MyPopup from "../MyPopup";
-import ConfirmPopup from "../ConfirmPopup";
 import Click from "./Click";
 import { lostIcon, foundIcon, finishedIcon, locationIcon } from "@/lib/icons";
-import { useState } from "react";
 
-export default function Map({ items, clickPosition, onPosition }) {
-  const router = useRouter();
-
-  const [isConfirmPopupOpen, setIsConfirmPopupOpen] = useState(false);
-
-  function handleConfirm() {
-    setIsConfirmPopupOpen(false);
-    router.push("/create");
-  }
-
-  function handleClose() {
-    setIsConfirmPopupOpen(false);
-  }
-
-  console.log(isConfirmPopupOpen);
+export default function Map({ items, onPosition }) {
   return (
     <StyledMapContainer
       center={[52.518623, 13.388517]}
       zoom={12}
       scrollWheelZoom
     >
-      <Click
-        onPosition={onPosition}
-        setIsConfirmPopupOpen={setIsConfirmPopupOpen}
-      />
-
-      {isConfirmPopupOpen && (
-        <Popup position={clickPosition} closeButton={false}>
-          <ConfirmPopup onConfirm={handleConfirm} onClose={handleClose} />
-        </Popup>
-      )}
-
+      <Click onPosition={onPosition} />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://api.maptiler.com/maps/bright-v2/{z}/{x}/{y}.png?key=LNcdyb0REf1mYIYz1asv"
