45530bcecabc36138358800c6a2b9eeeeac42b94
Olga Mishareva <mishareva.olga@gmail.com>
2023-02-13 14:47:43 +0100

    feat: add message form

---
+++ components/MessageForm.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+import SubmitButtonsSet from "@/components/SubmitButtonsSet";
+
+export default function MessageForm({
+  id,
+  isMutating,
+  onStopDiscuss,
+  inDiscuss,
+}) {
+  return (
+    <Form>
+      <Label htmlFor="message">Leave a message:</Label>
+      <Textarea
+        name="message"
+        id="message"
+        rows="3"
+        minLength="3"
+        maxLength="300"
+        autoFocus
+      ></Textarea>
+      <SubmitButtonsSet
+        variant="form"
+        type="submit"
+        link={id ? `/items/${id}` : "/"}
+        ariaLabel="cancel"
+        buttonName="Submit"
+        linkName="Cancel"
+        isMutating={isMutating}
+        onStopDiscuss={onStopDiscuss}
+        inDiscuss={inDiscuss}
+      />
+    </Form>
+  );
+}
+
+const Form = styled.form`
+  min-width: 18.5rem;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 0;
+`;
+
+const Label = styled.label`
+  margin: 0;
+  padding-bottom: 0.5rem;
+  font-size: 1rem;
+  font-weight: 500;
+  color: var(--grey-color);
+`;
+
+const Textarea = styled.textarea`
+  border: 1px solid var(--lightgrey-color);
+  border-radius: 0.5rem;
+  padding: 0.4rem;
+  font-family: var(--inter-font);
+  font-size: 1rem;
+  :focus {
+    border: 1px solid var(--found-color);
+    background-color: var(--input-color);
+  }
+`;
